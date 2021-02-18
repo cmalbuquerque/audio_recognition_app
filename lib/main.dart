32eb969aa26da7045c8e468ac0b7837eb9c541da
Carolina Albuquerque <carolinamalbuquerque29@gmail.com>
2021-02-18 22:04:01 +0000

    Example running in Android physical device

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tflite_audio/tflite_audio.dart';
 
 void main() {
   runApp(MyApp());
@@ -26,7 +27,44 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String _sound = "Press the button to start";
   bool _recording = false;
+  Stream<Map<dynamic, dynamic>> result;
 
+  @override
+  void initState() {
+    super.initState();
+    TfliteAudio.loadModel(
+        model: 'assets/soundclassifier.tflite',
+        label: 'assets/labels.txt',
+        numThreads: 1,
+        isAsset: true);
+  }
+
+  void _recorder() {
+    String recognition = "";
+    if (!_recording) {
+      setState(() => _recording = true);
+      result = TfliteAudio.startAudioRecognition(
+        numOfInferences: 1,
+        inputType: 'rawAudio',
+        sampleRate: 44100,
+        recordingLength: 44032,
+        bufferSize: 22016,
+      );
+      result.listen((event) {
+        recognition = event["recognitionResult"];
+      }).onDone(() {
+        setState(() {
+          _recording = false;
+          _sound = recognition.split(" ")[1];
+        });
+      });
+    }
+  }
+
+  void _stop() {
+    TfliteAudio.stopAudioRecognition();
+    setState(() => _recording = false);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +77,12 @@ class _MyHomePageState extends State<MyHomePage> {
               Container(
                 padding: EdgeInsets.all(20),
                 child: Text(
-                  "What's this sound?", style: Theme.of(context).textTheme.headline1,
+                  "What's this sound?",
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 70,
+                    fontWeight: FontWeight.w200,
+                  ),
                 ),
               ),
               Text(
@@ -47,7 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 style: Theme.of(context).textTheme.headline5,
               ),
               MaterialButton(
-                onPressed: (){},
+                onPressed: _recorder,
                 color: _recording ? Colors.grey : Colors.red,
                 textColor: Colors.white,
                 child: Icon(Icons.mic, size: 60),
